76119fa66737a501f1aec4dee38db3bec349efa4
Bukaeva Bayrta <bbembeeva@gmail.com>
2021-03-21 19:14:27 +0300

    отформотировала код

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        double weight=55;
-        double height=160;
+        double weight = 55;
+        double height = 160;
         double index = service.calculate(weight, height);
         System.out.println(index);
     }
